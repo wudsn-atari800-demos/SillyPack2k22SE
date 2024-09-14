6fd6450325f633c39c6179775be6d34fb61e01c3
Peter Dell <peter.dell@web.de>
2024-09-14 11:59:26 +0200

    Update image script and images

--- Make-Images.bat
+++ Make-Images.bat
@@ -8,40 +8,41 @@ IF ERRORLEVEL 1 goto :dir_error
 
 set TEMP_PNG=%TEMP%\SillyPack.png
 set RELEASE_FILE=%RELEASE%
+set MAGICK_OPTIONS=-define png:exclude-chunk=date 
 
 if not exist images goto :dir_error
 
 echo Resizing frame images.
 REM The first row has a smaller height of 200 pixels.
-%MAGICK% images\1.png -scale 336x240 -crop 336x200+0+17 %TEMP%\1.png
-%MAGICK% images\2.png -scale 336x240 -crop 336x200+0+20 %TEMP%\2.png
-%MAGICK% images\3.png -scale 336x240 -crop 336x200+0+20 %TEMP%\3.png
-%MAGICK% images\4.png -scale 336x240 %TEMP%\4.png
-%MAGICK% images\5.png -scale 336x240 %TEMP%\5.png
-%MAGICK% images\6.png -scale 336x240 %TEMP%\6.png
-%MAGICK% images\7.png -scale 336x240 %TEMP%\7.png
-%MAGICK% images\8.png -scale 336x240 %TEMP%\8.png
-%MAGICK% images\9.png -scale 336x240 %TEMP%\9.png
+%MAGICK% images\1.png %MAGICK_OPTIONS% -scale 336x240 -crop 336x200+0+17 %TEMP%\1.png
+%MAGICK% images\2.png %MAGICK_OPTIONS% -scale 336x240 -crop 336x200+0+20 %TEMP%\2.png
+%MAGICK% images\3.png %MAGICK_OPTIONS% -scale 336x240 -crop 336x200+0+20 %TEMP%\3.png
+%MAGICK% images\4.png %MAGICK_OPTIONS% -scale 336x240 %TEMP%\4.png
+%MAGICK% images\5.png %MAGICK_OPTIONS% -scale 336x240 %TEMP%\5.png
+%MAGICK% images\6.png %MAGICK_OPTIONS% -scale 336x240 %TEMP%\6.png
+%MAGICK% images\7.png %MAGICK_OPTIONS% -scale 336x240 %TEMP%\7.png
+%MAGICK% images\8.png %MAGICK_OPTIONS% -scale 336x240 %TEMP%\8.png
+%MAGICK% images\9.png %MAGICK_OPTIONS% -scale 336x240 %TEMP%\9.png
 
 echo Placing all 9 at the right position in the template.
-%MAGICK% composite -geometry  +0+0     %TEMP%\1.png images\Template.png %TEMP_PNG%
-%MAGICK% composite -geometry  +337+0   %TEMP%\2.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +674+0   %TEMP%\3.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +0+202   %TEMP%\4.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +337+202 %TEMP%\5.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +674+202 %TEMP%\6.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +0+443   %TEMP%\7.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +337+443 %TEMP%\8.png %TEMP_PNG%   %TEMP_PNG%
-%MAGICK% composite -geometry  +674+443 %TEMP%\9.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +0+0     %TEMP%\1.png images\Template.png %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +337+0   %TEMP%\2.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +674+0   %TEMP%\3.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +0+202   %TEMP%\4.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +337+202 %TEMP%\5.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +674+202 %TEMP%\6.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +0+443   %TEMP%\7.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +337+443 %TEMP%\8.png %TEMP_PNG%   %TEMP_PNG%
+%MAGICK% composite %MAGICK_OPTIONS% -geometry  +674+443 %TEMP%\9.png %TEMP_PNG%   %TEMP_PNG%
 
 echo Creating GIF of 336x226 pixels for the productions page preview (medium quality)
-%MAGICK% %TEMP_PNG% -scale 336x226 %RELEASE_FILE%.gif
+%MAGICK% %MAGICK_OPTIONS% %TEMP_PNG% -scale 336x226 %RELEASE_FILE%.gif
 
 echo Creating PNG of 336x226 pixels for the download package (high quality)
-%MAGICK% %TEMP_PNG% -scale 336x226 %RELEASE_FILE%.png
+%MAGICK% %MAGICK_OPTIONS% %TEMP_PNG% -scale 336x226 %RELEASE_FILE%.png
 
 echo Creating JPG of 400x270 pixels for Pouet (that is the maximum width allowed)
-%MAGICK% %TEMP_PNG% -scale 400x270 %RELEASE_FILE%.jpg
+%MAGICK% %MAGICK_OPTIONS% %TEMP_PNG% -scale 400x270 %RELEASE_FILE%.jpg
 goto :eof
 
 :dir_error
